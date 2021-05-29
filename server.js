d72def09f9b49fb2c95ee00ae6da3cb8a7979683
Robert Houeland <71792920+TheRobdog@users.noreply.github.com>
2021-05-28 23:14:11 -0700

    add mongodb

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-/*
- * Write your routing code in this file.  Make sure to add your name and
- * @oregonstate.edu email address below.
- *
- * Name: Robert Houeland
- * Email: houelanr@oregonstate.edu
- */
-
+require("dotenv").config()
 var path = require("path")
 var express = require("express")
 var exphbs = require("express-handlebars")
-var bodyParser = require("body-parser")
-
 var twitData = require("./twitData.json")
+var bodyParser = require("body-parser")
 
 var app = express()
 var port = process.env.PORT || 3000
+
+var mongoClient = require("mongodb").MongoClient
+var mongoUser = process.env.MONGO_USER
+var mongoPassword = process.env.MONGO_PASSWORD
+var mongoURL =
+    "mongodb+srv://" +
+    mongoUser +
+    ":" +
+    mongoPassword +
+    "@cluster0.etpfv.mongodb.net/test"
+
+var mongoDBDatabase
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
@@ -33,6 +37,19 @@ app.get("*", function (req, res) {
     res.status(404).render("404")
 })
 
-app.listen(port, function () {
-    console.log("== Server is listening on port", port)
-})
+mongoClient.connect(
+    mongoURL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    function (err, client) {
+        if (err) {
+            throw err
+        }
+        db = mongoDBDatabase = client.db("test")
+        app.listen(port, function () {
+            console.log("== Server is listening on port", port)
+        })
+        db.listCollections().toArray(function (err, collInfos) {
+            console.log(collInfos)
+        })
+    }
+)
